49ecbfde5b1397710b1b995c5e344422066786bf
Smirnov11 <iegor.smirnov.2004@mail.ru>
2024-02-19 12:55:50 +0300

    добавление комита

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sadkovGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 64, y = 8;
+            Console.WriteLine($"Вывод: {x/y}");
+            Console.ReadKey
+                ();
+        }
+    }
+}
